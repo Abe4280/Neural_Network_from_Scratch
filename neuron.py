db1519047a755f1ef611b13d7c96d8f6ace9b75a
Abri Kruger <pjakruger@protonmail.com>
2021-09-21 13:52:56 +0200

    Added a layer

--- neuron.py
+++ neuron.py
@@ -1,10 +1,25 @@
-# Basic working of a Neuron in a Neural Network
+# Basic working of a Neuron in a Neural Network.
 # Every neuron has a unique connection to every single previous neuron,
-# That serves as it's inputs.
-# In this case the neuron is connected to 3 previous neurons.
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3
+# that serves as it's inputs.
+# This is now a neuron in the output layer, with 4 previous neurons connected to it.
 
-output = (inputs[0]*weights[0]) + (inputs[1]*weights[1]) + (inputs[2]*weights[2]) + bias
+# You can never change inputs values directly. But, they can be be changed by changing their weights
+# and bias. This is how you tune the model.
+# This will be the output from the 4 previous neurons, just their weights and bias will change.
+inputs = [1, 2, 3, 2.5]
+# Every input has it's own unique weight.
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+# Each neuron just has one bias.
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+
+output = [
+    inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
+    inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
+    inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3
+]
 print(output)
